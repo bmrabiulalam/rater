f59f266734b0a5984b886effe4de25bb88ba41bb
Muhammad Rabiul Alam <bm.rabiulalam@gmail.com>
2021-07-11 10:33:51 +0600

    Modal Component Added with add and close button

--- styles/global.js
+++ styles/global.js
@@ -4,7 +4,7 @@ export const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-        // backgroundColor: '#fff'
+        backgroundColor: '#fff'
     },
     titleText: {
         fontFamily: 'nunito-regular',
